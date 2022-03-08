bf44b95bd97e93a8c16f6a2d60e2473922387178
Jason Dietrick <Jason_dietrick@hotmail.com>
2022-03-07 21:17:56 -0500

    climber

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,11 +5,15 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.subsystems.VisionSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.button.Button;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.subsystems.ClimberSubsystem;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.TapeVisionSubsystem;
@@ -30,14 +34,20 @@ public class RobotContainer {
   private final TurretShooterSubsystem turretShooter = new TurretShooterSubsystem();
   private final TurretSpinnerSubsystem turretSpinner = new TurretSpinnerSubsystem();
   private final IntakeSubsystem intake = new IntakeSubsystem();
+  private final ClimberSubsystem climb = new ClimberSubsystem();
   
 
   private final Command ballFollowerCommand = new BallFollowerCommand(drive, vision);
 
   public static XboxController driveController = new XboxController(0);
-  public static XboxController auxController = new XboxController(0);
+  public static XboxController auxController = new XboxController(1);
+
+  public final Joystick rightDriveStick = new Joystick(1);
 
   private final Trigger leftTrigger = new Trigger(intake::getLeftTrigger);
+
+  public final Button driveButtonX = new JoystickButton(driveController, Constants.xButtonDrive);
+  public final Button driveButtonB = new JoystickButton(driveController, Constants.bButtonDrive); 
 
 
 
@@ -47,13 +57,13 @@ public class RobotContainer {
     configureButtonBindings();
 
     
-    /*drive.setDefaultCommand(
+    drive.setDefaultCommand(
       new RunCommand(
         () -> drive.move(
           driveController.getRawAxis(0) * 0.6,
           driveController.getRawAxis(1) * 0.6
           ), drive
-    ));*/
+    ));
 
     turretShooter.setDefaultCommand(
       new RunCommand(
@@ -83,7 +93,15 @@ public class RobotContainer {
             intake.intakeBigwheelOff();
           }
           }, intake)
+
+          
     );
+    climb.setDefaultCommand(
+      new RunCommand(
+        () -> climb.moveElevator(
+         rightDriveStick.getRawAxis(0)
+        ), climb
+    ));
 
     
 
--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -4,11 +4,41 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class ClimberSubsystem extends SubsystemBase {
   /** Creates a new ClimberSubsystem. */
+  CANSparkMax elevatorMotor = new CANSparkMax(0, MotorType.kBrushless);
+  DoubleSolenoid climbPistonOne = new DoubleSolenoid(PneumaticsModuleType.REVPH, 1, 1);
+  DoubleSolenoid climbPistonTwo = new DoubleSolenoid(PneumaticsModuleType.REVPH, 1, 1);
+  Encoder elevatorEncoder = new Encoder(0, 0);
   public ClimberSubsystem() {}
+
+  public void pistonFoward(){
+    climbPistonOne.set(DoubleSolenoid.Value.kForward);
+    climbPistonTwo.set(DoubleSolenoid.Value.kForward);
+   }
+   public void pistonBackward(){
+     climbPistonOne.set(DoubleSolenoid.Value.kReverse);
+     climbPistonTwo.set(DoubleSolenoid.Value.kReverse);
+   }
+   public void pistonOff(){
+     climbPistonOne.set(DoubleSolenoid.Value.kOff);
+     climbPistonTwo.set(DoubleSolenoid.Value.kOff);
+   }
+  public void moveElevator(double speed){
+   elevatorMotor.set(speed);
+  }
+  public double getElevatorEncoder(){
+    return elevatorEncoder.get();
+   }
 
   @Override
   public void periodic() {
