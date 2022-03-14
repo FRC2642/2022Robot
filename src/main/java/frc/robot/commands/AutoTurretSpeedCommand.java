bf7ddcd6c35aa1588e2d82fb520d86ee313abb8b
Rylan <rytu10@icloud.com>
2022-03-13 23:16:53 -0400

    I forgot to change the isFinished to true

--- src/main/java/frc/robot/commands/AutoTurretSpeedCommand.java
+++ src/main/java/frc/robot/commands/AutoTurretSpeedCommand.java
@@ -53,6 +53,6 @@ public class AutoTurretSpeedCommand extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return true;
   }
 }
