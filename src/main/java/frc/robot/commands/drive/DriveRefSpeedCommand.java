f06f8e8fbd6cd7879a902a2f3e3d473167976f2f
thelightdesigner <48635344+thelightdesigner@users.noreply.github.com>
2022-04-07 18:16:30 -0400

    hmmm

---
+++ src/main/java/frc/robot/commands/drive/DriveRefSpeedCommand.java
@@ -0,0 +1,52 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.drive;
+
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.utils.MathR;
+
+public class DriveRefSpeedCommand extends CommandBase {
+  DriveSubsystem drive;
+  DoubleSupplier speed;
+  DoubleSupplier turn;
+  
+  public DriveRefSpeedCommand(DriveSubsystem drive, DoubleSupplier speed, DoubleSupplier turn) {
+    this.drive = drive;
+    this.speed = speed;
+    this.turn = turn;
+    addRequirements(drive);
+
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    drive.resetEncoder();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    drive.move(speed.getAsDouble(), turn.getAsDouble());
+    
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    drive.stop();
+  }
+
+  // Returns true when the command should end. 11027
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
